346afc02f94fa0b68b516747e1e9e3c9fc310591
Gabriel Henrique <gabrielbarros1704@gmail.com>
2025-10-30 11:47:00 -0300

    fetch: Adicionando codigo para criação das tabelas no DB

---
+++ SQL/tables.sql
@@ -0,0 +1,35 @@
+create database php_project;
+use php_project;
+
+create table students (
+	id int primary key,
+    name varchar(100),
+    date date,
+    sex enum('M', 'F'),
+	cpf int unique,
+	address varchar(255),
+    complement varchar(100),
+	cep int,
+    nearby varchar(255),
+    city varchar(255),
+    state varchar(70),
+    telephone int unique,
+    course_id int,
+    
+    foreign key(course_id) references courses(id) on delete set null 
+);
+
+create table courses(
+	id int auto_increment primary key,
+    name varchar(100),
+    description text,
+    release_date date
+);
+
+describe students;
+
+insert into students values (
+	 1, "test", 2025-02-12, 'M', 123456, "Moro em test, 333", "Casa", 9090, "Bairro Teste", "Cidade Test", "MG", 123423
+);
+
+select * from students;
