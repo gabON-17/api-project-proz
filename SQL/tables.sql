9700cd02b7e7b9f9c192824e013267c62c0653ab
Gabriel Henrique <gabrielbarros1704@gmail.com>
2025-11-06 19:21:14 -0300

    fetch: add em arquivo SQL com as tabelas teacher, students e course

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -2,28 +2,43 @@ create database php_project;
 use php_project;
 
 create table students (
-	id int primary key,
+	id int primary key auto_increment,
     name varchar(100),
-    date date,
+    birth date,
     sex enum('M', 'F'),
-	cpf varchar(11) unique,
+	cpf varchar(15) unique,
 	address varchar(255),
     complement varchar(100),
-	cep int,
+	cep varchar(10),
     nearby varchar(255),
     city varchar(255),
     state varchar(70),
-    telephone int unique,
+    telephone varchar(30) unique,
     course_id int,
+    createAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     
     foreign key(course_id) references courses(id) on delete set null 
+);
+
+create table teachers (
+	id int primary key auto_increment,
+    name varchar(100),
+    sex enum("M", "F"),
+    cpf varchar(15) unique,
+    telephone varchar(20) unique,
+	createAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 create table courses(
 	id int auto_increment primary key,
     name varchar(100),
     description text,
-    release_date date
+    creation_date date,
+    teacher int,
+	createAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 describe students;
@@ -32,4 +47,5 @@ insert into students values (
 	 1, "test", 2025-02-12, 'M', 123456, "Moro em test, 333", "Casa", 9090, "Bairro Teste", "Cidade Test", "MG", 123423
 );
 
+drop table students;
 select * from students;
